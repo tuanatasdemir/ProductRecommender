d9e04ccfc59714c2316f579c4561549f0359de04
tevana <tuanatasdemir02@gmail.com>
2026-01-03 21:30:45 +0300

    Initial commit: Product recommendation engine using Two Pointers

---
+++ ProductRecommender/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProductRecommender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Akıllı Ürün Öneri Sistemi (Two Pointers) ---\n");
+
+            int[] sortedPrices = { 50, 150, 200, 350, 500, 750, 1000, 1200, 1500, 2000 };
+            int targetBudget = 1350;
+
+            Console.WriteLine($"Mevcut Ürün Fiyatları: [{string.Join(", ", sortedPrices)}]");
+            Console.WriteLine($"Müşteri Bütçesi: {targetBudget} TL\n");
+
+            RecommendationEngine engine = new RecommendationEngine();
+            int[] resultIndices = engine.FindProductPair(sortedPrices, targetBudget);
+
+            if (resultIndices != null)
+            {
+                int index1 = resultIndices[0];
+                int index2 = resultIndices[1];
+                int price1 = sortedPrices[index1];
+                int price2 = sortedPrices[index2];
+
+                Console.WriteLine("✅ EŞLEŞME BULUNDU!");
+                Console.WriteLine($"Önerilen Paket: Ürün #{index1} ({price1} TL) + Ürün #{index2} ({price2} TL)");
+                Console.WriteLine($"Toplam: {price1 + price2} TL");
+            }
+            else
+            {
+                Console.WriteLine("❌ Bu bütçeye tam uygun ikili paket bulunamadı.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProductRecommender/RecommendationEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductRecommender
+{
+    public class RecommendationEngine
+    {
+        public int[] FindProductPair(int[] productPrices, int budget)
+        {
+            int left = 0;
+            int right = productPrices.Length - 1;
+
+            while (left < right)
+            {
+                int currentSum = productPrices[left] + productPrices[right];
+
+                if (currentSum == budget)
+                {
+                    return new int[] { left, right };
+                }
+                else if (currentSum < budget)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return null;
+        }
+    }
+}
